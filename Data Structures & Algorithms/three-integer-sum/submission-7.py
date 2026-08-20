b820b00f0c4c7476374d898317038161a776838c
Maxwell Klema <80615123+maxklema@users.noreply.github.com>
2026-08-20 00:42:56 -0500

    Add: three-integer-sum - submission-7

---
+++ Data Structures & Algorithms/three-integer-sum/submission-7.py
@@ -0,0 +1,25 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums = sorted(nums)  # O(nlogn)
+
+        res = set()
+
+        for pin_num in range(len(nums) - 2): #O(n)
+            if pin_num > 0 and nums[pin_num] == nums[pin_num - 1]:
+                continue
+
+            start = pin_num + 1
+            end = len(nums) - 1
+            target = 0 - nums[pin_num]
+            while (start < end): # O(n)
+                current = nums[end] + nums[start]
+                if (current == target):
+                    res.add(tuple([nums[pin_num], nums[start], nums[end]]))
+                    start += 1
+                    end -= 1
+                elif (current < target):
+                    start += 1
+                else:
+                    end -= 1
+        
+        return list(list(res))
