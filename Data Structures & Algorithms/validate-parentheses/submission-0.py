2b144b39e60646b80790c237faa675355a2bc05c
Maxwell Klema <80615123+maxklema@users.noreply.github.com>
2026-08-20 19:51:20 -0500

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        tagsOpen = {"{": "}", "[": "]", "(": ")"}
+        tagsClosed = {"}": "{", "]": "[", ")": "("}
+        stack = []
+        for tag in range(len(s)):
+            tag = s[tag]
+            if tag in tagsOpen.keys():
+                stack.append(tag)
+            elif len(stack) > 0 and stack[len(stack) - 1] == tagsClosed[tag]:
+                stack.pop()
+            else:
+                return False
+        if len(stack) > 0:
+            return False
+        return True
