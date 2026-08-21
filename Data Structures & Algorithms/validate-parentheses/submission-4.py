829180cb52cced5a60fc5c9a6928f6e28bae7b17
Maxwell Klema <80615123+maxklema@users.noreply.github.com>
2026-08-20 19:53:45 -0500

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        tags = {"}": "{", "]": "[", ")": "("}
+        stack = []
+        for tag in s:
+            if tag not in tags:
+                stack.append(tag)
+            elif len(stack) > 0 and stack[len(stack) - 1] == tags[tag]:
+                stack.pop()
+            else:
+                return False
+        if len(stack) > 0:
+            return False
+        return True
