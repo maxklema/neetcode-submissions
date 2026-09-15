bfcd21afec6e3c09e195cd515ac44f7744793d30
Maxwell Klema <80615123+maxklema@users.noreply.github.com>
2026-09-15 18:00:44 -0500

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.py
@@ -0,0 +1,40 @@
+class MinStack:
+    def __init__(self):
+        self.vals = {}
+        self.items = 0
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        self.vals[val] = self.vals.get(val, 0) + 1
+
+        if len(self.min_stack) == 0:
+            self.min_stack.append(val)
+        elif (self.min_stack[-1] >= val):
+            self.min_stack.append(val) 
+        self.items += 1           
+
+    def pop(self) -> None:
+        if (self.items == 0):
+            return
+
+        value = self.stack[-1]
+        self.stack.pop(-1)
+        if (self.min_stack[-1] == value):
+            self.min_stack.pop(-1)
+    
+        self.vals[value] = self.vals.get(value, 1) - 1
+        self.items -= 1
+
+    def top(self) -> int:
+        return self.stack[-1]
+  
+
+    def getMin(self) -> int:
+        while (self.vals[self.min_stack[-1]] == 0):
+            self.min_stack.pop(-1)
+
+        return self.min_stack[-1]
+
+        
